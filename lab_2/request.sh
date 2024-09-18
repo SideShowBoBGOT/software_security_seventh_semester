a0d8483602faf8d2bf7fd3fe568a3a13cf43e0b1
account <account@mail.com>
2024-09-18 11:10:47 +0300

    added lab_2

---
+++ lab_2/request.sh
@@ -0,0 +1,18 @@
+curl -s -L 'https://dev-88qrd45kn4mz6ptn.us.auth0.com/api/v2/users' \
+-H 'Content-Type: application/json' \
+-H 'Accept: application/json' \
+-H 'Authorization: Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImI0TURTYVhPclJsNjF4QWZPWktqMyJ9.eyJpc3MiOiJodHRwczovL2Rldi04OHFyZDQ1a240bXo2cHRuLnVzLmF1dGgwLmNvbS8iLCJzdWIiOiJ6ZHpoTjRkSURtSXVwdG5BclBoclo0VEVRQWtRTkdESEBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9kZXYtODhxcmQ0NWtuNG16NnB0bi51cy5hdXRoMC5jb20vYXBpL3YyLyIsImlhdCI6MTcyNjY0NTU0MCwiZXhwIjoxNzI2NzMxOTQwLCJzY29wZSI6InJlYWQ6dXNlcnMgY3JlYXRlOnVzZXJzIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIiwiYXpwIjoiemR6aE40ZElEbUl1cHRuQXJQaHJaNFRFUUFrUU5HREgifQ.g70JK26z2vB_7rOpDhdQNEh1J9cPLO2eghfuutTqxQHj9rlL59X08S6CNxsPnetsz7su9CHB6LHoVFAy06nCjZuc5JCz_6hpUdeUZsl0L4FPwe_EoH7Ag2WPKFzuBu-MCLY_uwlZhLAaQmgAns6e7DcK0BPeWrF8fEmY3pdXvOq3M_3xCAEFSPIDewl87vcRLeL9NZ_klOH24tMajzPARV3yzR3I2tSLzDT67JG0QltpiwNWa1XUaM6qD0k1zvrFuFsJjeGvre1l-ME1JvrolteRbHBNcfL9DnOUodvJJM6iQ_YVZVxELc-B-FgxQp8Nh05iTdnGsP2nqACq2a-xlw' \
+--data-raw '{
+    "email":"panchenko.serhii@lll.kpi.ua",
+    "user_metadata":{},
+    "blocked":false,
+    "email_verified":false,"app_metadata":{},
+    "given_name":"Serhii",
+    "family_name":"Panchenko",
+    "name":"Serhii",
+    "nickname":"Panchenko",
+    "picture":"https://sonicsuperstars.com/img/characters/sonic.png",
+    "connection":"Username-Password-Authentication",
+    "password":"IP11Panchenko",
+    "verify_email":false
+}'
